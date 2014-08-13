4bf924e5de92278f585bd5b005dd4074c03394db
ptsurko <pavel.tsurko@gmail.com>
2014-08-12 22:55:12 -0700

    updated timeline to use each functions

--- src/d3timeline.js
+++ src/d3timeline.js
@@ -1,8 +1,5 @@
 (function() {
-  if (!d3.starline) {
-    d3.starline = {};
-  }
-  d3.starline.timeline = function(options) {
+  d3.timeline = function(options) {
     var cfg = {
       h: 80,
       dateOffset: 5,
@@ -13,57 +10,60 @@
     var scale;
 
     function timeline(g) {
-      var timeline = g.selectAll('.timeline').data([0]);
-      timeline.enter()
-        .append('svg:g')
-        .attr('class', 'timeline');
+      g.each(function() {
+        var g = d3.select(this);
+        var timeline = g.selectAll('.timeline').data([0]);
+        timeline.enter()
+          .append('svg:g')
+          .attr('class', 'timeline');
 
-      var ticks = scale.ticks(cfg.tickCount);
-      var tickFormat = scale.tickFormat.call(scale, cfg.tickCount);
-      var tick = timeline.selectAll('.tick').data(ticks, scale);
-      var tickEnter = tick.enter()
-        .insert("g", ".domain").attr("class", "tick");
-      var tickExit = tick.exit().remove();
-      var tickUpdate = tick.order();
+        var ticks = scale.ticks(cfg.tickCount);
+        var tickFormat = scale.tickFormat.call(scale, cfg.tickCount);
+        var tick = timeline.selectAll('.tick').data(ticks, scale);
+        var tickEnter = tick.enter()
+          .insert("g", ".domain").attr("class", "tick");
+        var tickExit = tick.exit().remove();
+        var tickUpdate = tick.order();
 
-      tickEnter.append('svg:line');
-      tickEnter.append('svg:text');
+        tickEnter.append('svg:line');
+        tickEnter.append('svg:text');
 
-      var lineEnter = tickEnter.select('line');
-      var lineUpdate = tickUpdate.select('line');
+        var lineEnter = tickEnter.select('line');
+        var lineUpdate = tickUpdate.select('line');
 
-      var textEnter = tickEnter.select('text');
-      var textUpdate = tickUpdate.select('text');
-      var text = tick.select('text').text(tickFormat);
+        var textEnter = tickEnter.select('text');
+        var textUpdate = tickUpdate.select('text');
+        var text = tick.select('text').text(tickFormat);
 
-      lineEnter.attr('y2', cfg.innerTickSize);
-      lineUpdate.attr('y2', cfg.innerTickSize + 20)
-        .attr('x1', scale)
-        .attr('x2', scale);
+        lineEnter.attr('y2', cfg.innerTickSize);
+        lineUpdate.attr('y2', cfg.innerTickSize + 20)
+          .attr('x1', scale)
+          .attr('x2', scale);
 
-      textEnter.attr('y', cfg.innerTickSize + cfg.textPadding)
-        .attr("dy", ".71em");
-      textUpdate.attr('y', cfg.innerTickSize + cfg.textPadding)
-        .attr('x', function(d) { return scale(d) + cfg.textPadding;});
-
-
-      var range = scale.range();
-      var path = timeline.selectAll('.border').data([0]);
-      var pathEnter = path.enter()
-        .append('svg:path')
-        .attr('class', 'border')
-        .style('fill', 'none');
-
-      path.attr('d', 'M ' + range[0] + ' 0 H ' + range[1] + ' V ' + cfg.h + ' H ' + range[0] + ' V 0');
+        textEnter.attr('y', cfg.innerTickSize + cfg.textPadding)
+          .attr("dy", ".71em");
+        textUpdate.attr('y', cfg.innerTickSize + cfg.textPadding)
+          .attr('x', function(d) { return scale(d) + cfg.textPadding;});
 
 
-      var pathCenter = timeline.selectAll('.center').data([0]);
-      var pathCenterEnter = pathCenter.enter()
-        .append('svg:path')
-        .attr('class', 'center')
-        .style('fill', 'none');
+        var range = scale.range();
+        var path = timeline.selectAll('.border').data([0]);
+        var pathEnter = path.enter()
+          .append('svg:path')
+          .attr('class', 'border')
+          .style('fill', 'none');
 
-      pathCenter.attr('d', 'M ' + range[0] + ' ' + cfg.innerTickSize + ' H ' + range[1]);
+        path.attr('d', 'M ' + range[0] + ' 0 H ' + range[1] + ' V ' + cfg.h + ' H ' + range[0] + ' V 0');
+
+
+        var pathCenter = timeline.selectAll('.center').data([0]);
+        var pathCenterEnter = pathCenter.enter()
+          .append('svg:path')
+          .attr('class', 'center')
+          .style('fill', 'none');
+
+        pathCenter.attr('d', 'M ' + range[0] + ' ' + cfg.innerTickSize + ' H ' + range[1]);
+      });
     }
 
     timeline.scale = function(x) {
